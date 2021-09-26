5d382c16bd07429cb3761aa896b7dd9218b0cac3
HarishGowda <harishgowda2607@outlook.com>
2021-09-26 23:42:05 +0530

    Added log4j to the framework and log report, test output

--- SeleniumPOM/src/test/java/SeleniumFramework/SeleniumPOM/HomePageTestCases.java
+++ SeleniumPOM/src/test/java/SeleniumFramework/SeleniumPOM/HomePageTestCases.java
@@ -2,6 +2,8 @@ package SeleniumFramework.SeleniumPOM;
 
 import java.io.IOException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -21,12 +23,16 @@ import SeleniumFramework.SeleniumPOM.PageObjects.LandingPageComponent;
 import SeleniumFramework.SeleniumPOM.PageObjects.LoginPage;
 
 public class HomePageTestCases extends TestBase {
+	
+	public static Logger log=LogManager.getLogger(TestBase.class.getName());
 
 	@BeforeMethod
 	public void initialize() throws IOException {
 		driver = initialization();
+		log.info("Driver is initialized");
 
 		driver.get(prop.getProperty("url"));
+		log.info("Navigating to home page");
 	}
 
 	@Test(dataProvider = "getData")
@@ -39,6 +45,7 @@ public class HomePageTestCases extends TestBase {
 		Reporter.log("Validating the title of the page");
 
 		softAssert.assertTrue(title.contains("eBooks.com"), "Title of the page is as expected");
+		log.info("Successfully validated the title of the page");
 
 		LandingPageComponent landingPageComp = new LandingPageComponent(driver);
 
