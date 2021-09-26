48c283fbc29ada93675fcbeaaf076c59a2c5b18d
HarishGowda <harishgowda2607@outlook.com>
2021-09-26 10:19:35 +0530

    Added test cases for Home page and LandingPage

--- SeleniumPOM/src/main/java/SeleniumFramework/SeleniumPOM/PageObjects/LandingPageComponent.java
+++ SeleniumPOM/src/main/java/SeleniumFramework/SeleniumPOM/PageObjects/LandingPageComponent.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LandingPageComponent {
-	
+
 	private WebDriver driver;
 
 	public LandingPageComponent(WebDriver driver) {
@@ -20,12 +20,54 @@ public class LandingPageComponent {
 	public WebElement getLogin() {
 		return login;
 	}
-	
+
 	@FindBy(xpath = "//a[contains(text(),'Browse')]")
 	WebElement browse;
 
 	public WebElement getBrowse() {
 		return browse;
+	}
+
+	@FindBy(xpath = "//a[contains(text(),'Gift Cert')]")
+	WebElement giftCertificate;
+
+	public WebElement getGiftCertificate() {
+		return giftCertificate;
+	}
+
+	@FindBy(xpath = "//a[contains(text(),'DRM-Free Books')]")
+	WebElement drmFreeBooks;
+
+	public WebElement getDrmFreeBooks() {
+		return drmFreeBooks;
+	}
+
+	@FindBy(xpath = "//a[contains(text(),'Feedback')]")
+	WebElement feedback;
+
+	public WebElement getFeedback() {
+		return feedback;
+	}
+
+	@FindBy(xpath = "//a[@href='https://about.ebooks.com/']")
+	WebElement aboutUs;
+
+	public WebElement getAboutUs() {
+		return aboutUs;
+	}
+
+	@FindBy(xpath = "//div[@class='pull-right']//a[contains(text(),'Create')]")
+	WebElement createNewUser;
+
+	public WebElement getCreateNewUser() {
+		return createNewUser;
+	}
+
+	@FindBy(xpath = "//span[@id='account-menu-dropdown']")
+	WebElement profile;
+
+	public WebElement getProfile() {
+		return profile;
 	}
 
 }
--- SeleniumPOM/src/test/java/SeleniumFramework/SeleniumPOM/HomePageTestCases.java
+++ SeleniumPOM/src/test/java/SeleniumFramework/SeleniumPOM/HomePageTestCases.java
@@ -10,45 +10,56 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import SeleniumFramework.SeleniumPOM.PageObjects.LandingPage;
+import SeleniumFramework.SeleniumPOM.PageObjects.LandingPageComponent;
 import SeleniumFramework.SeleniumPOM.PageObjects.LoginPage;
 
-public class HomePageTestCases extends TestBase{
-	
+public class HomePageTestCases extends TestBase {
+
 	@Test
 	public void login() throws IOException, InterruptedException {
-		driver=initialization();
-		
+		driver = initialization();
+
 		driver.get("https://ebooks.com/");
-		
-		LandingPage landingPage=new LandingPage(driver);
-		
-		ExpectedConditions.elementToBeClickable(landingPage.getLogin());
-		
-		//Explicit wait while using page factory model
-		//https://stackoverflow.com/a/54177337/17003989
-		
-		WebElement loginElement = new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(landingPage.getLogin()));
-		
-		landingPage.getLogin().click();
-		
+
+		LandingPageComponent landingPageComp = new LandingPageComponent(driver);
+
+		ExpectedConditions.elementToBeClickable(landingPageComp.getLogin());
+
+		// Explicit wait while using page factory model
+		// https://stackoverflow.com/a/54177337/17003989
+
+		WebElement loginElement = new WebDriverWait(driver, 5)
+				.until(ExpectedConditions.elementToBeClickable(landingPageComp.getLogin()));
+
+		landingPageComp.getLogin().click();
+
 //		Thread.sleep(5000);
-		
-		LoginPage loginPage=new LoginPage(driver);
-		
+
+		LoginPage loginPage = new LoginPage(driver);
+
 		loginPage.getUsername().sendKeys("harishgowdas2607@gmail.com");
-		
+
 		Thread.sleep(1000);
-		
+
 //		Actions actions=new Actions(driver);
 //		
 //		actions.sendKeys(Keys.ENTER).build().perform();
-		
+
 		loginPage.getContinueBtn().click();
-		
+
 		loginPage.getPassword().sendKeys("PracticeDaily10");
-		
+
 		loginPage.getSignIn().click();
 		
+		landingPageComp.getBrowse().click();
+		
+		landingPageComp.getFeedback().click();
+		
+		WebElement profileLink = new WebDriverWait(driver, 5)
+				.until(ExpectedConditions.elementToBeClickable(landingPageComp.getProfile()));
+		
+		landingPageComp.getProfile().click();
+
 	}
 
 }
